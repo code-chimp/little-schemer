327bc3c135068c3bf467892b83236d483ed2faf2
Tim Goshinski <tim@code-chimp.com>
2014-10-09 23:06:48 -0500

    what's up w/indentation?

--- tls.clj
+++ tls.clj
@@ -92,80 +92,80 @@
 ; purely academic exercise as the following would
 ; fail badly if m is negative
 (defn plus [n m]
-    (cond
-        (zero? m) n
-        :else (plus (add1 n) (sub1 m))))
+  (cond
+    (zero? m) n
+    :else (plus (add1 n) (sub1 m))))
 
 (defn minus [n m]
-    (cond
-        (zero? m) n
-        :else (minus (sub1 n) (sub1 m))))
+  (cond
+    (zero? m) n
+    :else (minus (sub1 n) (sub1 m))))
 
 (defn x [n m]
-    (cond
-        (zero? m) m
-        :else (+ n (x n (sub1 m)))))
+  (cond
+    (zero? m) m
+    :else (+ n (x n (sub1 m)))))
 
 (defn ↑ [n m]
-    (cond
-        (zero? m) 1
-        :else (* n (↑ n (sub1 m)))))
+  (cond
+    (zero? m) 1
+    :else (* n (↑ n (sub1 m)))))
 
 ; this is clean though
 (defn addtup [tup]
-    (cond
-        (null? tup) 0
-        :else (+ (first tup) (addtup (rest tup)))))
+  (cond
+    (null? tup) 0
+    :else (+ (first tup) (addtup (rest tup)))))
 
 (defn tup+ [tup1 tup2]
-    (cond
-        (null? tup1) tup2
-        (null? tup2) tup1
-        :else (cons (+ (first tup1) (first tup2))
-                    (tup+ (rest tup1) (rest tup2)))))
+  (cond
+    (null? tup1) tup2
+    (null? tup2) tup1
+    :else (cons (+ (first tup1) (first tup2))
+                (tup+ (rest tup1) (rest tup2)))))
 
 (defn myDiv [n m]
-    (cond
-        (< n m) 0
-        :else (add1 (myDiv (- n m) m))))
+  (cond
+    (< n m) 0
+    :else (add1 (myDiv (- n m) m))))
 
 (defn length [lat]
-    (cond
-        (null? lat) 0
-        :else (add1 (length (rest lat)))))
+  (cond
+    (null? lat) 0
+    :else (add1 (length (rest lat)))))
 
 (defn pick [n lat]
-    (cond
-        (null? lat) nil
-        (zero? (sub1 n)) (first lat)
-        :else (pick (sub1 n) (rest lat))))
+  (cond
+    (null? lat) nil
+    (zero? (sub1 n)) (first lat)
+    :else (pick (sub1 n) (rest lat))))
 
 (defn rempick [n lat]
-    (cond
-        (null? lat) nil
-        (= n 1) (rest lat)
-        :else (cons (first lat)
-                    (rempick (sub1 n) (rest lat)))))
+  (cond
+    (null? lat) nil
+    (= n 1) (rest lat)
+    :else (cons (first lat)
+                (rempick (sub1 n) (rest lat)))))
 
 (defn no-nums [lat]
-    (cond
-        (null? lat) []
-        (number? (first lat)) (no-nums (rest lat))
-        :else (cons (first lat)
-                    (no-nums (rest lat)))))
+  (cond
+    (null? lat) []
+    (number? (first lat)) (no-nums (rest lat))
+    :else (cons (first lat)
+                (no-nums (rest lat)))))
 
 (defn all-nums [lat]
-    (cond
-        (null? lat) []
-        (not (number? (first lat))) (all-nums (rest lat))
-        :else (cons (first lat)
-                    (all-nums (rest lat)))))
+  (cond
+    (null? lat) []
+    (not (number? (first lat))) (all-nums (rest lat))
+    :else (cons (first lat)
+                (all-nums (rest lat)))))
 
 (defn occur [a lat]
-    (cond
-        (null? lat) 0
-        (= (first lat) a) (add1 (occur a (rest lat)))
-        :else (occur a (rest lat))))
+  (cond
+    (null? lat) 0
+    (= (first lat) a) (add1 (occur a (rest lat)))
+    :else (occur a (rest lat))))
 
 (defn rember* [a l]
   (cond
